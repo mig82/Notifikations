9ab963b7634faef3fafd2a2dafa61faac06bd677
Miguelángel Fernández <miguelangelxfm@gmail.com>
2019-07-09 12:32:57 +0200

    Change namespace and clean up external dependencies

---
+++ userwidgets/com.mig82.Notifikation/modules/require/NotifikationController.js
@@ -0,0 +1,16 @@
+define(function() {
+	//Notifikation v2.0
+	return {
+		preShow: function(){},
+
+		constructor: function(/*baseConfig, layoutConfig, pspConfig*/) {
+			this.view.onTouchEnd = () => {
+				//TODO
+			};
+		},
+		//Logic for getters/setters of custom properties
+		initGettersSetters: function() {
+
+		}
+	};
+});
